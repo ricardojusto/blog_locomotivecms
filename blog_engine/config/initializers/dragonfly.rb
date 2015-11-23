c6d7cd2988a9709c4d7c87a402b5c6f03b54ec57
ricardojusto <ricardojusto@outlook.com>
2015-11-23 14:11:01 +0000

    first commit

---
+++ blog_engine/config/initializers/dragonfly.rb
@@ -0,0 +1,24 @@
+require 'dragonfly'
+
+# Configure
+Dragonfly.app.configure do
+  plugin :imagemagick,
+    convert_command:  `which convert`.strip.presence || '/usr/local/bin/convert',
+    identify_command: `which identify`.strip.presence || '/usr/local/bin/identify'
+
+  verify_urls true
+
+  secret '3a4881e0a39bb51ef071710c55bd985b34261bc8fb1313736bf33ae17811d4dc'
+
+  url_format '/images/dynamic/:job/:basename.:ext'
+
+  fetch_file_whitelist /public/
+
+  fetch_url_whitelist /.+/
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware
---
+++ blog_engine/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogEngine::Application.config.secret_token = '7e46f814cf7262ac03e00454712864912654511c943271b8b5f9065537b1734148dc0962750c0848404915d338d139573b67f66f4e7512c9aba37b45b37a0e41'
